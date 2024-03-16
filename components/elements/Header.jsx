e201e5874f13e71ca80b19be4c7fc10ea05a4638
ErikUTA <erik4tohj@gmail.com>
2024-03-16 08:38:57 -0600

    feat(blog): change some styles

--- components/elements/Header.jsx
+++ components/elements/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
 
     return (
       <Grid className="navbar center">
-        <img className='logo-header' src="../../src/assets/movies.png" />
+        <img className='logo-header' src="https://i.pinimg.com/originals/10/b1/ba/10b1bad21f79df5f462c1c09f12db6ff.jpg" />
         <Link to={'/listOpinions'} className="menu-item">
           Lista de comentarios
         </Link>
